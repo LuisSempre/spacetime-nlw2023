40c2d0751a3300adca10b70ae0b2ac2e3d4a52b3
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-16 01:39:37 -0300

    day1

---
+++ client/app.vue
@@ -0,0 +1,7 @@
+<template>
+<div class='bg-black w-full h-screen'>
+  <div class='flex justify-center items-center mx-auto max-w-2xl text-white'>
+    <h1>Spacetime with NuxtJS</h1>
+  </div>
+  </div>
+</template>
