8d32d4542535195e99942a3076be1beba079ca4f
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-17 15:34:08 -0300

    Add HeroIcons

--- client/app.vue
+++ client/app.vue
@@ -6,49 +6,49 @@
       <div
         class="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 blur-4xl"
       />
-      <div class="absolute bottom-0 right-0.5 top-0 w-2 bg-stripes border-white/10 border-r" />
+      <div class="absolute bottom-0 right-0 top-0 w-2 bg-stripes border-white/10 border-r" />
       <a
         href=""
-        className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        class="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
       >
-        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-          <User class-name="h-5 w-5 text-gray-500" />
+        <div class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200">
+          <UserIcon class="h-5 w-5 text-gray-500" />
         </div>
 
-        <p className="max-w-[140px] text-sm leading-snug">
-          <span className="underline">Create your account</span> and save your
+        <p class="max-w-[140px] text-sm leading-snug">
+          <span class="underline">Create your account</span> and save your
           memoirs
         </p>
       </a>
-      <div className="space-y-5">
+      <div class="space-y-5">
         <img
-          src="{nlwLogo}"
+          src="@/assets/nlw-spacetime-logo.svg"
           alt="NLW Spacetime"
         >
 
-        <div className="max-w-[420px] space-y-1">
-          <h1 className="text-5xl font-bold leading-tight text-gray-50">
+        <div class="max-w-[420px] space-y-1">
+          <h1 class="text-5xl font-bold leading-tight text-gray-50">
             Your time capsule
           </h1>
-          <p className="text-lg leading-relaxed">
+          <p class="text-lg leading-relaxed">
             Collect remarkable moments of your journey and share (if want) with the world!
           </p>
         </div>
 
         <a
-          className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          class="inline-block rounded-full bg-purple-600 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
           href=""
         >
           REGISTER MEMORY
         </a>
       </div>
 
-      <div className="text-sm leading-relaxed text-gray-200">
+      <div class="text-sm leading-relaxed text-gray-200">
         Create with 💚 in NLW by
         <a
           target="_blank"
           rel="noreferrer"
-          className="underline hover:text-gray-100"
+          class="underline hover:text-gray-100"
           href="https://github.com/LuisSempre"
         >
           Luis Lucas
@@ -67,3 +67,6 @@
     </div>
   </main>
 </template>
+<script setup>
+	import { UserIcon } from '@heroicons/vue/solid'
+</script>
