a263ffb3b15012a662d7f4e3987d2c74eb921bf4
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-18 19:31:46 -0300

    Day 4

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -35,9 +35,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       >
         <main className="grid min-h-screen grid-cols-2">
           {/* Left */}
-          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
             {/* Blur */}
-            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-green-700 opacity-50 blur-full" />
 
             {/* Stripes */}
             <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
@@ -48,7 +48,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex flex-col p-16">
             {children}
           </div>
         </main>
--- web/src/components/Copyright.tsx
+++ web/src/components/Copyright.tsx
@@ -1,14 +1,14 @@
 export function Copyright() {
   return (
     <div className="text-sm leading-relaxed text-gray-200">
-      Feito com 💜 no NLW da{' '}
+      By&nbsp;
       <a
         target="_blank"
         rel="noreferrer"
         className="underline hover:text-gray-100"
-        href="https://rocketseat.com.br"
+        href="https://github.com/LuisSempre"
       >
-        Rocketseat
+        Luis Lucas
       </a>
     </div>
   )
