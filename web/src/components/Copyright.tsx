330d4187108a47a52b5313df7922cd8db342a09e
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-20 00:09:51 -0300

    Up

--- web/src/components/Copyright.tsx
+++ web/src/components/Copyright.tsx
@@ -1,14 +1,14 @@
 export function Copyright() {
   return (
     <div className="text-sm leading-relaxed text-gray-200">
-      By&nbsp;
+      Feito com 💜 no NLW da{' '}
       <a
         target="_blank"
         rel="noreferrer"
         className="underline hover:text-gray-100"
-        href="https://github.com/LuisSempre"
+        href="https://rocketseat.com.br"
       >
-        Luis Lucas
+        Rocketseat
       </a>
     </div>
   )
--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -10,11 +10,11 @@ export function Hero() {
 
       <div className="max-w-[420px] space-y-1">
         <h1 className="text-5xl font-bold leading-tight text-gray-50">
-          Your time capsule
+          Sua cápsula do tempo
         </h1>
         <p className="text-lg leading-relaxed">
-         Collect remarkable moments from your journey and share (if you want)
-           with the world!
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo!
         </p>
       </div>
 
@@ -22,7 +22,7 @@ export function Hero() {
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
         href="/memories/new"
       >
-        REGISTER MEMORY
+        CADASTRAR LEMBRANÇA
       </Link>
     </div>
   )
